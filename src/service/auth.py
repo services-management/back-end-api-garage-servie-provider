d6865b31a609a6aa95b5b9984ad09957b2f9ea4f
albkue <albkue@example.com>
2026-01-19 17:32:30 +0700

    add screct key and algorithm

--- src/service/auth.py
+++ src/service/auth.py
@@ -9,8 +9,8 @@ from jose import JWTError, jwt
 load_dotenv()
 # --- CONFIGURATION (Set secure, unique keys) ---
 # NOTE: Replace 'YOUR_SECRET_KEY_HERE' with a real, long, random key loaded from env vars!
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY","TmFl9_ch01kORiplQKsWev9tE0WmXyZg1uuGzq2OZcU")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 # 1 day
 
 def create_access_token(data: Dict[str, Any], expires_delta: Optional[timedelta] = None) -> str:
