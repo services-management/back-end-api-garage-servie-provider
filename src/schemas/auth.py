64f2f1818775e518cc3126c860e436d7ba308e16
albkue <albkue@example.com>
2026-03-21 14:25:53 +0700

    add refesh token

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -4,4 +4,6 @@ from pydantic import BaseModel
 class Token(BaseModel):
     """Schema for the JWT access token response."""
     access_token: str
+    refresh_token: str
     token_type: str = "bearer"
+    expires_in: int
